086e6d0e2f059b73a47c8642c9421f1f68c91363
Stephen Goncalves <stiefo.o@gmail.com>
2025-12-10 16:41:07 +0000

    middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
  
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     if (request.nextUrl.pathname.startsWith("/auth") || request.nextUrl.pathname.startsWith("/api/auth")) {
         return NextResponse.next();
     }
